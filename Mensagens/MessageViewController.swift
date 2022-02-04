a453bc85e5c71ecb4f1953c36e16aeea929e8f8b
renatogirao <renato.sgirao@gmail.com>
2022-02-03 23:10:08 -0300

    making glasses inherit the apply color protocol

--- Mensagens/MessageViewController.swift
+++ Mensagens/MessageViewController.swift
@@ -9,9 +9,39 @@ import UIKit
 
 class MessageViewController: BaseViewController {
 
+    var labelTeste: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        message = Message()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! MessageColorViewController
+        vc.message = message
+    }
+    
+    override func changeColor(_ sender: UIButton) {
+        let ColorPickerVC = storyboard?.instantiateViewController(withIdentifier: "ColorPickerViewController") as! ColorPickerViewController
+        ColorPickerVC.modalPresentationStyle = .overCurrentContext
+        ColorPickerVC.reference = self
+        present(ColorPickerVC, animated: true, completion: nil)
+    }
 }
 
+extension MessageViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        message.text = textField.text!
+        labelMessage.text = textField.text!
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension MessageViewController: colorPickerProtocol {
+    func applyColor(color: UIColor) {
+        labelMessage.textColor = color
+        message.textColor = color
+    }
+}
