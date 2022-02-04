54103486417a257c08e52aeac1757ca5291a4aab
renatogirao <renato.sgirao@gmail.com>
2022-02-03 23:11:34 -0300

    created the extension of the class and inheriting the protocol

--- Mensagens/MessageColorViewController.swift
+++ Mensagens/MessageColorViewController.swift
@@ -11,7 +11,21 @@ class MessageColorViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelMessage.text = message.text
+        labelMessage.textColor = message.textColor
     }
     
+    override func changeColor(_ sender: UIButton) {
+        let colorPickerView = storyboard?.instantiateViewController(withIdentifier: "ColorPickerViewController") as! ColorPickerViewController
+        colorPickerView.modalPresentationStyle = .overCurrentContext
+        present(colorPickerView, animated: true, completion: nil)
+    }
+}
+
+extension MessageColorViewController: colorPickerProtocol {
+    func applyColor(color: UIColor) {
+        labelMessage.textColor = color
+        message.textColor = color
+    }
 }
 
